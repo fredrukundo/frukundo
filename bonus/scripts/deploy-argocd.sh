ae822afbc9276cb64ab474a5348e0aa45d3d001f
frukundo <dukefred9@gmail.com>
2026-01-29 03:03:54 +0100

    updates

--- bonus/scripts/deploy-argocd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "== Deploying Argo CD bonus config =="
-
-kubectl apply -f bonus/confs/argocd-project.yaml
-kubectl apply -f bonus/confs/argocd-application.yaml
-
-echo "Argo CD application deployed"
